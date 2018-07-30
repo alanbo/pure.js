de40450ddb0007ad5c02edbfe23e63c60fc59603
alan.bogusiewicz <alan.bogusiewicz@camppacific.com>
2018-07-30 00:06:46 -0700

    adds each method

--- src/Pure.js
+++ src/Pure.js
@@ -1,4 +1,38 @@
+// @flow
+
+import type {
+  PureAdded,
+  ArrayLikePure,
+  HtmlElemListPure,
+  HtmlElemPure,
+  HtmlCollectionPure,
+  HtmlAll
+} from './types/html-types';
+
+
 class Pure {
+  +elem: HtmlAll;
+
+  constructor(elem: HtmlAll) {
+    this.elem = elem;
+  }
+
+  each(callback: (html_element: HTMLElement) => void) {
+    if (this.elem instanceof HTMLCollection || this.elem instanceof NodeList) {
+      for (let i = 0; i < this.elem.length; i++) {
+        const pure_elem = this.elem[i];
+        pure_elem.$ = new Pure(this.elem[i]);
+        callback(this.elem[i]);
+      }
+    } else if (this.elem instanceof HTMLElement) {
+      callback(this.elem);
+    } else {
+      console.log('You are calling .each() method on an empty element');
+    }
+
+    return this;
+  }
+
 
 }
 
